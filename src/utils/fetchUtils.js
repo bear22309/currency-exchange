aa0abc53456a7736769a6525defa22a29c98b04c
Tony <anthonymostrowski@gmail.com>
2024-05-06 11:50:10 -0400

    updated files

--- src/utils/fetchUtils.js
+++ src/utils/fetchUtils.js
@@ -1,14 +1,9 @@
-// fetchUtils.js
-
-// Utility function to check the status of a fetch response
 export const checkStatus = (response) => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
+  if (response.ok) {
+    // .ok returns true if response status is 200-299
     return response;
-  };
-  
-  // Utility function to parse JSON response
-  export const json = (response) => response.json();
-  
-  
+  }
+  throw new Error('Request was either a 404 or 500');
+}
+
+export const json = (response) => response.json()
